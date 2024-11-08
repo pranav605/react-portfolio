eb65f4a597346c64f513b9789043e2cbacc0eece
pranav605 <pranav605@gmail.com>
2024-11-07 20:36:42 -0500

    Created file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen bg-gray-900">
+      <h2 class="md:text-5xl font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">Sai Pranav Nishtala</h2>
     </div>
   );
 }
